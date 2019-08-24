3ee0e85ce3f2ccfe5c62cd6b65971b4b49e70aec
Isobel.nixon <inix0001@student.monash.edu.au>
2019-08-24 20:34:54 +1000

    Everything is shit and I hate my life

--- bit_by_bit_app/src/App.js
+++ bit_by_bit_app/src/App.js
@@ -64,12 +64,14 @@ class App extends React.Component{
       </header>
       <body>
         <Section headerName = "Target_char" name = "Target: " cond = {true}><Cube></Cube></Section>
-        <Section headerName = "Current_char" name = "Current: " cond = {true}><Cube></Cube></Section>
-        <Section headerName = "Partner_char" name = "Partner: " cond = {true}><Cube></Cube></Section>
+        <div className = "sideBySide">
+          <Section id = "left" headerName = "Current_char" name = "Current: " cond = {true}><Cube></Cube></Section>
+          <Section id = "right"headerName = "Partner_char" name = "Partner: " cond = {true}><Cube></Cube></Section>
+        </div>
         <Section headerName = "Match_chars" name = "" cond = {true}>
           <Button buttonName = "Match!" toggle = {this.state.show_child}></Button>
         </Section>
-        <Section className="Children_chars" headerName = "Children_chars" name = "Offspring" cond = {this.state.show_child}></Section>
+        <Section className="Children_chars" headerName = "Children_chars" name = "Offspring" cond = {this.state.show_child}><Cube></Cube></Section>
       </body>
     </div>
   );
