29c3ddbf8372bfeed278b046291a034c5591fd89
Isobel.nixon <inix0001@student.monash.edu.au>
2019-08-24 20:34:54 +1000

    Everything is shit and I hate my life

--- bit_by_bit_app/src/App.js
+++ bit_by_bit_app/src/App.js
@@ -64,13 +64,15 @@ class App extends React.Component{
         <h1 idName = "Title">Pixel Labs</h1>
       </header>
       <body>
-        <Section className="section_center" headerName = "Target_char" name = "Target: "></Section>
-        <Section className="section_left" headerName = "Current_char" name = "Current: "></Section>
-        <Section className="section_right" headerName = "Partner_char" name = "Partner: "></Section>
-        <Section className="section_center" headerName = "Match_chars" name = "">
-        <Button caption = "Matched!"/>
+        <Section headerName = "Target_char" name = "Target: " cond = {true}><Cube></Cube></Section>
+        <div className = "sideBySide">
+          <Section id = "left" headerName = "Current_char" name = "Current: " cond = {true}><Cube></Cube></Section>
+          <Section id = "right"headerName = "Partner_char" name = "Partner: " cond = {true}><Cube></Cube></Section>
+        </div>
+        <Section headerName = "Match_chars" name = "" cond = {true}>
+          <Button buttonName = "Match!" toggle = {this.state.show_child}></Button>
         </Section>
-        <Section className="Children_chars" headerName = "Children_chars" name = "Offspring" cond = {this.state.show_child}></Section>
+        <Section className="Children_chars" headerName = "Children_chars" name = "Offspring" cond = {this.state.show_child}><Cube></Cube></Section>
       </body>
     </div>
   );
