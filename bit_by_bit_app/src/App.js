74b5be25ce86c573f50733781654bf9cfb10edee
Isobel.nixon <inix0001@student.monash.edu.au>
2019-08-24 13:48:07 +1000

    Made header names more presentable

--- bit_by_bit_app/src/App.js
+++ bit_by_bit_app/src/App.js
@@ -5,21 +5,34 @@ class Section extends React.Component{
   constructor(props){
     super(props);
   }
-
-
   render(){
   return(
-    <div className = "SubSections">
+    <div idName = {this.props.headerName}>
     <h2 className = "subheader">
-      {this.props.headerName}
+      {this.props.name}
     </h2>
+    {this.props.children}
     </div>
   )
   }
 }
 
-function onClick(){
-
+class Button extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {isToggleOn: false};
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(){
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+  render(){
+    return(
+      <button idName = "button" onClick = {this.handleClick}>Match!</button>
+    )
+  }
 }
 
 function App() {
@@ -29,14 +42,16 @@ function App() {
         <h1 idName = "Title">App Title</h1>
       </header>
       <body>
-        <Section headerName = "Target_char"></Section>
-        <Section headerName = "Current_char"></Section>
-        <Section headerName = "Partner_char"></Section>
-        <button idName = "Match" onClick = {this.onClick}>Match!</button>
-        <Section headerName = "Children"></Section>
+        <Section headerName = "Target_char" name = "Target: "></Section>
+        <Section headerName = "Current_char" name = "Current: "></Section>
+        <Section headerName = "Partner_char" name = "Partner: "></Section>
+        <Section headerName = "Match_chars" name = "">
+          <Button buttonName = "Match!"></Button>
+        </Section>
+        <Section headerName = "Children_chars" name = "Offspring"></Section>
       </body>
     </div>
   );
-}
+  }
 
 export default App;
