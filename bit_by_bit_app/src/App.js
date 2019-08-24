a0a64c4bfca86adbcd1746c699b631ebbb3f7751
Isobel.nixon <inix0001@student.monash.edu.au>
2019-08-24 12:37:18 +1000

    Deleted base code

--- bit_by_bit_app/src/App.js
+++ bit_by_bit_app/src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 
+
+class Section extends React.Component{
+
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 idName = "Title">App Title</h1>
       </header>
+      <body>
+        <div idName = "Saved_Characters">
+          <h2 className = "subheader">
+            Saved characters
+          </h2>
+        </div>
+      </body>
     </div>
   );
 }
