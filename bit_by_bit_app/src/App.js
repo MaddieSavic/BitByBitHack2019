902cddc2e1c4a09fa746c97d725d07ef190473fb
Isobel.nixon <inix0001@student.monash.edu.au>
2019-08-24 19:35:58 +1000

    useable button kinda

--- bit_by_bit_app/src/App.js
+++ bit_by_bit_app/src/App.js
@@ -3,32 +3,40 @@ import './App.css';
 
 class Section extends React.Component{
   render(){
-  return(
-    <div idName = {this.props.headerName}>
+    if(this.props.cond){
+      return(
+      <div idName = {this.props.headerName}>
     <h2 className = "subheader">
       {this.props.name}
     </h2>
     {this.props.children}
     </div>
-  )
+      );
+    }
+    else{
+      return null;
+    }
   }
 }
 //For any button
 class Button extends React.Component{
   constructor(props){
     super(props);
-    this.state = {isToggleOn: false};
+    this.state = {isToggleOn: false, show_child: this.props.show_child};
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick(){
-    this.setState(state => ({
-      isToggleOn: !state.isToggleOn
-    }));
+    alert("Match was clicked");
+    this.setState(state=> ({show_child: !state.show_child}));
   }
   //html of button
   render(){
     return(
-      <ul><li onClick = {this.handleClick}>Match!</li></ul>
+      <div className = "btn">
+        <a onClick = {this.handleClick}>Match!</a>
+        <h2>{this.state.isToggleOn}</h2>
+      </div>
+      
     )
   }
 }
@@ -36,28 +44,36 @@ class Button extends React.Component{
 class Cube extends React.Component{
   render(){
     return(
-      <div className = "Box"></div>
+      <div className = "Box">
+        <h2>This will be a cube</h2>
+      </div>
     );
   }
 }
 
-function App() {
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state ={show_child: false}
+  }
+  render(){
   return (
     <div className="App">
       <header className="App-header">
-        <h1 idName = "Title">Meet Your Match!</h1>
+        <h1 id = "Title">Meet Your Match!</h1>
       </header>
       <body>
-        <Section headerName = "Target_char" name = "Target: "><Cube></Cube></Section>
-        <Section headerName = "Current_char" name = "Current: "></Section>
-        <Section headerName = "Partner_char" name = "Partner: "></Section>
-        <Section headerName = "Match_chars" name = "">
-          <Button buttonName = "Match!"></Button>
+        <Section headerName = "Target_char" name = "Target: " cond = {true}><Cube></Cube></Section>
+        <Section headerName = "Current_char" name = "Current: " cond = {true}><Cube></Cube></Section>
+        <Section headerName = "Partner_char" name = "Partner: " cond = {true}><Cube></Cube></Section>
+        <Section headerName = "Match_chars" name = "" cond = {true}>
+          <Button buttonName = "Match!" toggle = {this.state.show_child}></Button>
         </Section>
-        <Section className="section_center" headerName = "Children_chars" name = "Offspring"></Section>
+        <Section className="Children_chars" headerName = "Children_chars" name = "Offspring" cond = {this.state.show_child}></Section>
       </body>
     </div>
   );
+  }
   }
 
 export default App;
