8e0c55fb7d81d8b2581511057b9fc103b84ff432
MaddieSavic <30588636+MaddieSavic@users.noreply.github.com>
2019-08-25 10:21:53 +1000

    delete unnessacry bootstrap import

--- bit_by_bit_app/src/index.js
+++ bit_by_bit_app/src/index.js
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
@@ -8,4 +8,3 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
